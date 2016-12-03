597a0bbfa81826421c8e1373a819d7db2603f646
Supreeth S <supreeth90@gmail.com>
2016-12-03 02:01:59 -0500

    Topics2 working

--- topics2.py
+++ topics2.py
@@ -81,8 +81,8 @@ def load_train_data(dataset_dir, fraction):
             for filename in files:
                 file_path = os.path.join(current_dir_path, filename)
                 file_fd = open(file_path, 'r')
-                # words = preprocess_data(file_path)
-                words = file_fd.read(-1).split()
+                words = preprocess_data(file_path)
+                # words = file_fd.read(-1).split()
                 doc_name = dir_name + "_" + filename
                 if doc_name not in doc_dict:
                     doc_dict[doc_name] = init_doc_dict()
@@ -95,9 +95,15 @@ def load_train_data(dataset_dir, fraction):
                     doc_dict[doc_name]['WORDS'][word] += 1
                     doc_dict[doc_name]['TOTAL_WORDS'] += 1
 
-                    # Check if this can be labelled
-                    cur_num = random.randint(1, 10)
-                    if cur_num <= fraction * 10:
+                # Check if this can be labelled
+                cur_num = random.randint(1, 10)
+                if cur_num <= fraction * 10:
+                    doc_dict[doc_name]['TOPICS'][topic_name] = 1.0
+                    doc_dict[doc_name]['LABELLED'] = True
+                    doc_dict[doc_name]['LABEL'] = topic_name
+                elif fraction == 0.0:
+                    if cur_num <= 1:
+                        topic_name = topics_list[random.randint(0,19)]
                         doc_dict[doc_name]['TOPICS'][topic_name] = 1.0
                         doc_dict[doc_name]['LABELLED'] = True
                         doc_dict[doc_name]['LABEL'] = topic_name
@@ -109,7 +115,7 @@ def preprocess_topic_word(doc_dict):
 
     for doc_name in doc_dict:
         if doc_dict[doc_name]['LABELLED']:
-            topic_name = doc_name.split("_")[0]
+            topic_name = doc_dict[doc_name]['LABEL']
             words = doc_dict[doc_name]['WORDS']
             total_words = doc_dict[doc_name]['TOTAL_WORDS']
 
@@ -133,32 +139,32 @@ def preprocess_topic_word(doc_dict):
     return topic_word_dict
 
 
-def build_topic_word_dict_from_dir(dataset_dir):
-    topic_word_dict = dict()
-
-    for dir_name in os.listdir(dataset_dir):
-        current_dir_path = os.path.join(dataset_dir, dir_name)
-        topic_name = dir_name
-        # total_words_in_topic = 0
-        # doc_word_count_dict = dict()
-
-        if topic_name not in topic_word_dict:
-            topic_word_dict[topic_name] = dict()
-
-        if os.path.isdir(current_dir_path):
-            for filename in os.listdir(current_dir_path):
-                file_path = os.path.join(current_dir_path, filename)
-                file_fd = open(file_path, 'r')
-                # words = preprocess_data(file_path)
-                words = file_fd.read(-1).split()
-
-                for word in words:
-                    # print(doc_dict[doc_name])
-                    if word not in topic_word_dict[topic_name]:
-                        topic_word_dict[topic_name][word] = 0
-                    topic_word_dict[topic_name][word] += 1
-                    # total_words_in_topic += 1
-    return topic_word_dict
+# def build_topic_word_dict_from_dir(dataset_dir):
+#     topic_word_dict = dict()
+#
+#     for dir_name in os.listdir(dataset_dir):
+#         current_dir_path = os.path.join(dataset_dir, dir_name)
+#         topic_name = dir_name
+#         # total_words_in_topic = 0
+#         # doc_word_count_dict = dict()
+#
+#         if topic_name not in topic_word_dict:
+#             topic_word_dict[topic_name] = dict()
+#
+#         if os.path.isdir(current_dir_path):
+#             for filename in os.listdir(current_dir_path):
+#                 file_path = os.path.join(current_dir_path, filename)
+#                 file_fd = open(file_path, 'r')
+#                 # words = preprocess_data(file_path)
+#                 words = file_fd.read(-1).split()
+#
+#                 for word in words:
+#                     # print(doc_dict[doc_name])
+#                     if word not in topic_word_dict[topic_name]:
+#                         topic_word_dict[topic_name][word] = 0
+#                     topic_word_dict[topic_name][word] += 1
+#                     # total_words_in_topic += 1
+#     return topic_word_dict
 
 
 # return { 'Topic1': prob1, 'Topic2': prob2 }
@@ -213,11 +219,6 @@ def label_docs(doc_dict, topic_word_dict, topic_doc_dict):
                     prob_topic_dict[topic] += math.log((topic_word_dict[topic][word]+1.0)/(topic_word_dict[topic]['TOTAL_WORDS']+len(topic_word_dict[topic])))
                 else:
                     prob_topic_dict[topic] += math.log(1.0/(topic_word_dict[topic]['TOTAL_WORDS']+len(topic_word_dict[topic])))
-                    # prob_topic_dict[topic] += topic_word_dict[topic][word]
-                    # prob_topic_dict[topic] += math.log((float(doc_dict[doc_name]['WORDS'][word])/doc_dict[doc_name]['TOTAL_WORDS'])*topic_word_dict[topic][word])
-                # else:
-                #     prob_topic_dict[topic] += math.log((float(doc_dict[doc_name]['WORDS'][word])/doc_dict[doc_name]['TOTAL_WORDS'])*float(random.randint(1,10))/1.0)
-
         sorted_topics = sorted(prob_topic_dict.items(), key=operator.itemgetter(1), reverse=True)
         final_result.append((doc_name.split("_")[0], sorted_topics[0][0]))
     return final_result
@@ -229,11 +230,9 @@ def main():
     dataset_dir = sys.argv[2]
     model_file = sys.argv[3]
     fraction = float(sys.argv[4])
-    distinctive_words_file = "distinctive words.txt"
+    distinctive_words_file = "distinctive_words.txt"
     doc_dict = dict()
-    retrain_times = 1
-    out_data = []
-    print("-----")
+    retrain_times = 5
 
     if mode == "train":
         doc_dict, topic_doc_dict = load_train_data(dataset_dir, fraction)
@@ -245,30 +244,20 @@ def main():
                 print("Hi !!!")
                 doc_dict, topic_word_dict, topic_doc_dict = retrain(doc_dict, topic_word_dict, topic_doc_dict)
 
-
-            # Process probability
-            # for word in topic_word_dict[topic]:
-            #     topic_word_dict[topic][word] = float(topic_word_dict[topic][word])/total_words_in_topic
-
-            # Probability based on number of documents had a word to number of documents in the topic
-            #     total_documents = len(os.listdir(current_dir_path))
-            #     for word in doc_word_count_dict:
-            #         topic_word_dict[topic][word] = float(len(doc_word_count_dict[word]))/total_documents
-
-
-        #
-        # dis_fd = open(distinctive_words_file, 'w')
-        # for topic in topic_word_dict:
-        #     words_topic = topic_word_dict[topic]
-        #     dis_word_string = topic
-        #     sorted_words = sorted(words_topic.items(), key=operator.itemgetter(1), reverse=True)
-        #     # print(sorted_words)
-        #     for word_tuple in sorted_words[0:10]:
-        #         dis_word_string += ":"+word_tuple[0]
-        #     dis_word_string += "\n"
-        #     print(dis_word_string)
-        #     dis_fd.write(dis_word_string)
-        # dis_fd.close()
+        dis_fd = open(distinctive_words_file, 'w')
+        for topic in topic_word_dict:
+            words_topic = topic_word_dict[topic]
+            dis_word_string = topic+":"
+            sorted_words = sorted(words_topic.items(), key=operator.itemgetter(1), reverse=True)
+            # print(sorted_words)
+            for word_tuple in sorted_words[0:11]:
+                if word_tuple[0] != 'TOTAL_WORDS':
+                    dis_word_string += word_tuple[0]+","
+            dis_word_string = dis_word_string[:-1]
+            print(dis_word_string)
+            dis_word_string += "\n"
+            dis_fd.write(dis_word_string)
+        dis_fd.close()
 
         # Store the model
         with open(model_file, 'w') as f:
@@ -276,64 +265,15 @@ def main():
                         pickle.HIGHEST_PROTOCOL)
 
     elif mode == "test":
-
         with open(model_file, 'r') as f:
             topic_word_dict, topic_doc_dict = pickle.load(f)
 
-        # total_words = 0
-        # for topic in topic_word_dict:
-        #     for word in topic_word_dict[topic]:
-        #         total_words += topic_word_dict[topic][word]
-        # print("total_words:" + str(total_words))
-
-        doc_dict, test_word_dict = load_train_data(dataset_dir, fraction)
+        doc_dict, test_word_dict = load_train_data(dataset_dir, 1.0)
         out_data = label_docs(doc_dict, topic_word_dict, topic_doc_dict)
-        # print out_data
         display_accuracy(accuracy(out_data))
-        # print(out_data)
-
-
-
-        # Read the model
-
-
-        # for dirname in os.listdir(dataset_dir):
-        #     current_dir_path = os.path.join(dataset_dir, dirname)
-        #
-        #     if os.path.isdir(current_dir_path):
-        #         for filename in os.listdir(current_dir_path):
-        #             total_for_topic_dict = dict()
-        #             file_path = os.path.join(current_dir_path, filename)
-        #             # print filename
-        #             file_fd = open(file_path, 'r')
-        #             # words = preprocess_data(file_path)
-        #             words = file_fd.read(-1).split()
-        #             for word in words:
-        #                 for topic in topic_word_dict:
-        #                     if word in topic_word_dict[topic]:
-        #                         if topic not in total_for_topic_dict:
-        #                             total_for_topic_dict[topic] = 0
-        #
-        #                         total_for_topic_dict[topic] += math.log(topic_word_dict[topic][word])
-        #             sorted_topics = sorted(total_for_topic_dict.items(), key=operator.itemgetter(1))
-        #             print(file_path, sorted_topics[0])
-
     else:
         print("Invalid Mode")
 
 
 if __name__ == '__main__':
     main()
-
-# print topic_word_dict['guns']['encryption']
-# print len(topic_word_dict['guns'])
-# print new_word_dict['guns']['encryption']
-# print topic_word_dict['crypto']['encryption']
-
-
-
-
-
-# x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
-# sorted_x = sorted(x.items(), key=operator.itemgetter(1), reverse=True)
-# print(sorted_x)
